59e9623a578ea9c4540e77bcfba8af891b6b4846
Lennart Oymanns <lennart.oymanns@rwth-aachen.de>
2012-07-04 16:45:33 +0200

    pdf particle numbers added
    there are now more particle numbers from the monte carlo particle
    numbering scheme in package pdg

--- hep/pdg/pdg.go
+++ hep/pdg/pdg.go
@@ -5,12 +5,14 @@ const (
 	// PDG codes for particles
 	// anti particles are denoted with a minus, e.g. anti d quark: -DQuark
 	// Quarks
-	DQuark = 1
-	UQuark = 2
-	SQuark = 3
-	CQuark = 4
-	BQuark = 5
-	TQuark = 6
+	DQuark      = 1
+	UQuark      = 2
+	SQuark      = 3
+	CQuark      = 4
+	BQuark      = 5
+	TQuark      = 6
+	BQuarkPrime = 7
+	TQuarkPrime = 8
 	// Leptons
 	Electron         = 11
 	ElectronNeutrino = 12
@@ -18,14 +20,22 @@ const (
 	MuonNeutrino     = 14
 	Tau              = 15
 	TauNeutrino      = 16
+	TauPrime         = 17
+	TauPrimeNeutrino = 18
 	// Gauge
 	Gluon  = 21
 	Photon = 22
 	Z      = 23
 	Wplus  = 24
 	// Higgs
-	Higgs = 25
-	//SUSY
+	Higgs       = 25
+	ZPrime      = 32
+	ZPrimePrime = 33
+	WPrime      = 34
+	H0          = 35
+	A0          = 36
+	HPlus       = 37
+	// SUSY
 	SDQuarkL           = 1000001
 	SUQuarkL           = 1000002
 	SSQuarkL           = 1000003
@@ -46,4 +56,22 @@ const (
 	Neutralino4        = 1000035
 	Chargino1          = 1000024
 	Chargino2          = 1000037
+	// Light I=1 Mesons
+	Pi0    = 111
+	PiPlus = 211
+	// Light I=0 Mesons
+	Eta      = 221
+	EtaPrime = 331
+	// Strange Mesons
+	Kaon     = 311
+	KaonPlus = 321
+	// cc~ Mesons
+	JPsi = 443
+	// Light Baryons
+	Proton        = 2212
+	Neutron       = 2112
+	DeltaPlusPlus = 2224
+	DeltaPlus     = 2114
+	Delta0        = 2114
+	DeltaMinus    = 1114
 )
